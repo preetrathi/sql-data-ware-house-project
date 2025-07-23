7b24e13ad4f8e4fb4ae4c6974a2ecee3e018ff78
Preet Kumar <40492888+preetrathi@users.noreply.github.com>
2025-07-24 00:52:32 +0500

    Update bronze_layer.sql
    Creating crm_cust_info table

--- scripts/bronze_layer.sql
+++ scripts/bronze_layer.sql
@@ -5,9 +5,26 @@
 Step 01: Analyzing (Interview source system expert)
 	Before building bronze layer, we have to understand the source system. its better to get understanding of source system from source system expert
 	and ask them many many questions in order to understand the nature of source system.
+
+	some questions
+		Q1: Who owns the data?
+		Q2: What business process it support? e.g: Customer TRANSACTIONS, supply chain or logistics
+		Q3: System & Data documentation?
+		Q4: Data model & data catalog? e.g desc of columns and tables will help alot in datawarehouse, and join tables togather.
+		Q5: Architecture & Technology Stack?
+			* How is data stored? e.g: SQL server, Oracle, AWS, Azure etc.
+			* What are the integration capibilities? e.g: API, Kafka, File Extract, DB etc.
+		Q6: Extract and Load?
+			* Increamental vs Full Loads?
+			* Data Scope & Historical needs
+			* What is the expected size of the extraction?
+			* Are there any data volume limitations? e.g some old system which stuggle in performance
+			* How to avoid impacting the source system's performance?
+			* Authentication & Authorization?
 	
 Step 02: Coding (Data Ingestion)
 	Find a way how to load data from source to datawarehouse. 
+	
 
 Step 03: Data Validation (Data Completeness & Schema Checks)
 	Its very important in the bronze layer to check the data completeness, so we have to compare the number od records between source system and the bronze
@@ -16,3 +33,26 @@ Step 03: Data Validation (Data Completeness & Schema Checks)
 
 Step 04: Docs and Version (Data documenting & versioning in git.)
 */
+
+
+-- Create DDL script for the Bronze layer
+/*
+	For creating tables of bronze layer, we have to understand the metadata the structure the schema of the incoming data.
+
+	Data Profiling:
+		Explore the data to identify column names & data types.
+
+*/
+
+CREATE TABLE bronze.crm_cust_info (
+
+-- columns names should be excatly same as source system
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_martial_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
+						
