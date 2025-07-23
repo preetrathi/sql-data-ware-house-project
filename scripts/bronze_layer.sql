69634607a0dad125f0e2455aea1115c6455fec13
Preet Kumar <40492888+preetrathi@users.noreply.github.com>
2025-07-24 00:20:48 +0500

    Update bronze_layer.sql

--- scripts/bronze_layer.sql
+++ scripts/bronze_layer.sql
@@ -1,1 +1,18 @@
 
+-- Building Bronze Layer
+
+/*
+Step 01: Analyzing (Interview source system expert)
+	Before building bronze layer, we have to understand the source system. its better to get understanding of source system from source system expert
+	and ask them many many questions in order to understand the nature of source system.
+	
+Step 02: Coding (Data Ingestion)
+	Find a way how to load data from source to datawarehouse. 
+
+Step 03: Data Validation (Data Completeness & Schema Checks)
+	Its very important in the bronze layer to check the data completeness, so we have to compare the number od records between source system and the bronze
+	layer just to make sure we are not losing any data in between.
+	Other check is Schema check, that make sure that the data is placed on the right position.
+
+Step 04: Docs and Version (Data documenting & versioning in git.)
+*/
